1c084bc975b9626875bd0b972cf9d1620a82d429
yangg205 <167543231+yangg205@users.noreply.github.com>
2024-11-11 02:14:20 +0700

    up

---
+++ Assets/Standard Assets/Utility/FPSCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI; // Thêm dòng này
+
+namespace UnityStandardAssets.Utility
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        const float fpsMeasurePeriod = 0.5f;
+        private int m_FpsAccumulator = 0;
+        private float m_FpsNextPeriod = 0;
+        private int m_CurrentFps;
+        const string display = "{0} FPS";
+        [SerializeField] private Text m_Text; // Thay GUIText bằng Text và thêm SerializeField để gắn trong Inspector
+
+        private void Start()
+        {
+            m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
+        }
+
+        private void Update()
+        {
+            // measure average frames per second
+            m_FpsAccumulator++;
+            if (Time.realtimeSinceStartup > m_FpsNextPeriod)
+            {
+                m_CurrentFps = (int)(m_FpsAccumulator / fpsMeasurePeriod);
+                m_FpsAccumulator = 0;
+                m_FpsNextPeriod += fpsMeasurePeriod;
+                if (m_Text != null) // Kiểm tra nếu m_Text đã được gán
+                {
+                    m_Text.text = string.Format(display, m_CurrentFps);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI; // Thêm dòng này
+
+namespace UnityStandardAssets.Utility
+{
+    public class SimpleActivatorMenu : MonoBehaviour
+    {
+        // Một menu rất đơn giản khi nhận tham chiếu đến các GameObject trong scene
+        public Text camSwitchButton; // Thay GUIText bằng Text
+        public GameObject[] objects;
+
+        private int m_CurrentActiveObject;
+
+        private void OnEnable()
+        {
+            // active object bắt đầu từ đối tượng đầu tiên trong mảng
+            m_CurrentActiveObject = 0;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+
+        public void NextCamera()
+        {
+            int nextActiveObject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetActive(i == nextActiveObject);
+            }
+
+            m_CurrentActiveObject = nextActiveObject;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+    }
+}
