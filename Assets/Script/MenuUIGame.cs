21fd27e1e05a602fc93a05ac5d2796b12f5221ab
yangg205 <167543231+yangg205@users.noreply.github.com>
2024-11-27 16:39:13 +0700

    up

--- Assets/Script/MenuUIGame.cs
+++ Assets/Script/MenuUIGame.cs
@@ -17,10 +17,20 @@ public class MenuUIGame : MonoBehaviour
     public void StartGame()
     {
         //chuyen sang scene game
+        Debug.Log("da vo game");
         UnityEngine.SceneManagement.SceneManager.LoadScene("map");
     }
     public void Option()
     {
         optioninner.SetActive(true);
+    }
+    public void ExitOption()
+    {
+        optioninner.SetActive(false);
+    }
+    public void Exit()
+    {
+        Debug.Log("da thoat game");
+        Application.Quit();
     }
 }
---
+++ Assets/Script/Volume.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    //[SerializeField] private AudioSource audio;
+    [SerializeField] private Slider musicSlider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //musicSlider.value = 0.8f;
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = musicSlider.value;
+    }
+    private void Load()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    private void save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
+    }
+
+}
