418ec44975a61bbfc8e21fbcf7dd07cb06792381
duyyyyyyyyy <duytnps41276@gmail.com>
2024-11-30 17:09:01 +0700

    duy
    duy

---
+++ Assets/Script/LightSwitch.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject inttext, light;
+    public bool toggle = true, interactable;
+    public Renderer lightBulb;
+    public Material offlight, onlight;
+    public AudioSource lightSwitchSound;
+    public Animator switchAnim;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            inttext.SetActive(true);
+            interactable = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            inttext.SetActive(false);
+            interactable = false;
+        }
+    }
+    void Update()
+    {
+        if (interactable == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                toggle = !toggle;
+                //lightSwitchSound.Play();
+                switchAnim.ResetTrigger("press");
+                switchAnim.SetTrigger("press");
+            }
+        }
+        if (toggle == false)
+        {
+            light.SetActive(false);
+            lightBulb.material = offlight;
+        }
+        if (toggle == true)
+        {
+            light.SetActive(true);
+            lightBulb.material = onlight;
+        }
+    }
+}
